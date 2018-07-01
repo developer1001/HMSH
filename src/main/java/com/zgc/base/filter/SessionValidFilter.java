b83cc0eaacb1d14b7e501e739280830029e5595e
developer1001 <yangpeng_188@sina.com>
2018-07-01 23:11:58 +0800

    加入登录拦截器，springMvc的拦截器

--- src/main/java/com/zgc/base/filter/SessionValidFilter.java
+++ src/main/java/com/zgc/base/filter/SessionValidFilter.java
@@ -16,12 +16,21 @@ public class SessionValidFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-        HttpSession sess = ((HttpServletRequest)request).getSession();
-        SysUser loginUser = (SysUser) sess.getAttribute("loginUser");
-        if(loginUser == null ){
-            ((HttpServletResponse)response).sendRedirect(((HttpServletRequest)request).getContextPath()+"/view/login.jsp");
+//        HttpSession sess = ((HttpServletRequest)request).getSession();
+//        SysUser loginUser = (SysUser) sess.getAttribute("loginUser");
+//        if(loginUser == null ){
+//            ((HttpServletResponse)response).sendRedirect(((HttpServletRequest)request).getContextPath()+"/view/login.jsp");
+//        }else{
+//            filterChain.doFilter(request, response);
+//        }
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        HttpSession session = req.getSession();
+        if(session.getAttribute("loginUser") == null){
+            req.getRequestDispatcher("/view/login.jsp").forward(req, resp);
+
         }else{
-            filterChain.doFilter(request, response);
+            filterChain.doFilter(req,resp);
         }
     }
 
