1a36794c3fbf77b9b7d73bc3f7a43aece9dc050e
developer1001 <yangpeng_188@sina.com>
2018-07-01 23:36:37 +0800

    response

--- src/main/java/com/zgc/base/controller/BaseController.java
+++ src/main/java/com/zgc/base/controller/BaseController.java
@@ -1,21 +1,42 @@
 package com.zgc.base.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletWebRequest;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Writer;
 
 /**
- *
+ *controller的父类，子类可以继承
  */
+@Controller
 public class BaseController {
+
     /**
      * 对象转json
      * @param object
      * @param response
      */
     public void writeJson(Object object, HttpServletResponse response){
+        String json = JSONObject.toJSONString(object);
+        System.out.print(json);
+        response.setContentType("text/html;charset=UTF-8");
+        try {
+            Writer writer = response.getWriter();
+            writer.write(json);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeJson2(Object object){
+        HttpServletResponse response = ((ServletWebRequest)RequestContextHolder.getRequestAttributes()).getResponse();
         String json = JSONObject.toJSONString(object);
         System.out.print(json);
         response.setContentType("text/html;charset=UTF-8");
