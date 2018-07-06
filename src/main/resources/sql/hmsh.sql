4e7639976dfb1f50b0f9e8a0109aa60f7e766fa0
developer1001 <31823231+developer1001@users.noreply.github.com>
2018-07-06 18:38:58 +0800

    sql文件更新

--- src/main/resources/sql/hmsh.sql
+++ src/main/resources/sql/hmsh.sql
@@ -1,39 +1,139 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50522
+Source Server         : zs
+Source Server Version : 50717
 Source Host           : localhost:3306
 Source Database       : hmsh
 
 Target Server Type    : MYSQL
-Target Server Version : 50522
+Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2018-07-01 16:01:04
+Date: 2018-07-06 18:34:19
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for sys_auth
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_auth`;
+CREATE TABLE `sys_auth` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '菜单id',
+  `menuName` varchar(50) DEFAULT NULL COMMENT '菜单名称',
+  `menuCode` varchar(50) DEFAULT NULL COMMENT '菜单编码',
+  `menuUrl` varchar(50) DEFAULT NULL COMMENT '菜单地址',
+  `description` varchar(50) DEFAULT NULL COMMENT '菜单描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_auth
+-- ----------------------------
+INSERT INTO `sys_auth` VALUES ('1', 'menu1', null, null, 'luanmaya');
+INSERT INTO `sys_auth` VALUES ('2', 'menu2', 'asgbsdgkjs564dfgsd', 'https://www.baidu.com', 'ä¹±ç å');
+
+-- ----------------------------
+-- Table structure for sys_dept
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_dept`;
+CREATE TABLE `sys_dept` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `dept_name` varchar(50) DEFAULT NULL COMMENT '部门名称',
+  `dept_code` varchar(50) DEFAULT NULL COMMENT '部门编码',
+  `pid` int(10) DEFAULT NULL COMMENT '上级部门id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_dept
+-- ----------------------------
+INSERT INTO `sys_dept` VALUES ('1', 'dept1', 'asgbsdgkjs564dfgsd', '0');
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `roleCode` varchar(50) DEFAULT NULL COMMENT '角色编码',
+  `roleName` varchar(50) DEFAULT NULL COMMENT '角色名称',
+  `description` varchar(50) DEFAULT NULL COMMENT '角色描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES ('1', 'g55w1qwh6', 'role1', 'è§è²1');
+
+-- ----------------------------
+-- Table structure for sys_role_auth
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_auth`;
+CREATE TABLE `sys_role_auth` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `roleId` int(10) DEFAULT NULL COMMENT '角色ID',
+  `authId` int(10) DEFAULT NULL COMMENT '菜单ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_role_auth
+-- ----------------------------
+INSERT INTO `sys_role_auth` VALUES ('1', '12', '45');
 
 -- ----------------------------
 -- Table structure for sys_user
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(40) NOT NULL,
-  `loginName` varchar(40) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `department_id` int(10) DEFAULT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `userName` varchar(40) NOT NULL COMMENT '用户名',
+  `loginName` varchar(40) NOT NULL COMMENT '登录名',
+  `password` varchar(100) NOT NULL COMMENT '密码',
   PRIMARY KEY (`id`),
   UNIQUE KEY `loginName` (`loginName`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES ('1', 'admin', 'admin', 'e10adc3949ba59abbe56e057f20f883e', null);
-INSERT INTO `sys_user` VALUES ('2', 'superadmin', 'superadmin', 'e10adc3949ba59abbe56e057f20f883e', null);
+INSERT INTO `sys_user` VALUES ('1', 'admin', 'admin', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `sys_user` VALUES ('2', 'superadmin', 'superadmin', 'e10adc3949ba59abbe56e057f20f883e');
+
+-- ----------------------------
+-- Table structure for sys_user_dept
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_dept`;
+CREATE TABLE `sys_user_dept` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `userId` int(10) DEFAULT NULL COMMENT '用户ID',
+  `deptId` int(10) DEFAULT NULL COMMENT '部门ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_user_dept
+-- ----------------------------
+INSERT INTO `sys_user_dept` VALUES ('1', '125', '453');
+
+-- ----------------------------
+-- Table structure for sys_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `userId` int(10) DEFAULT NULL COMMENT '用户id',
+  `roleId` int(10) DEFAULT NULL COMMENT '角色id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_user_role
+-- ----------------------------
+INSERT INTO `sys_user_role` VALUES ('1', '5', '56');
 
 -- ----------------------------
 -- Table structure for t_user
